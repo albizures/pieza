53cd372274f4b1196e9450f2b4dc08a81d1f4b02
Jose Albizures <albizures3601@gmail.com>
2020-03-13 17:32:05 +0300

    first commit 🚀

---
+++ examples/basic/src/piezas/index.ts
@@ -0,0 +1,9 @@
+import { create, useContext } from '@pieza/core';
+
+export default create({
+	name: 'My title',
+	setup() {
+		const context = useContext();
+		context.circle(10, 10, 10);
+	},
+});
