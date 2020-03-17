507d1c665f0682bcee1ebf3eaac59fd4324d14ca
Jose Albizures <albizures3601@gmail.com>
2020-03-17 10:12:56 +0300

    docs(examples): create example using settings

--- examples/src/piezas/settings.ts
+++ examples/src/piezas/settings.ts
@@ -4,6 +4,7 @@ import {
 	useContext,
 	PiezaSettings,
 	useSettings,
+	BooleanSetting,
 	StringSetting,
 	NumberSetting,
 } from '@pieza/core';
@@ -11,17 +12,33 @@ import {
 interface Settings extends PiezaSettings {
 	value: number;
 	text: string;
+	size: number;
+	isDoubleSize: boolean;
 }
 
 export default create<Settings>({
 	name: 'Using the settings',
 	setup() {
-		const { value } = useSettings<Settings>();
+		const { value, size, isDoubleSize, text } = useSettings<Settings>();
 		const context = useContext();
 
-		context.circle(value, value, 50);
+		context.circle(value, value, isDoubleSize ? size * 2 : size);
+		context.textSize(30);
+		context.text(text, 50, 50);
 	},
 	settings: {
+		size: NumberSetting({
+			label: 'Size',
+			slide: true,
+			value: 50,
+			min: 10,
+			max: 100,
+			step: 10,
+		}),
+		isDoubleSize: BooleanSetting({
+			label: 'is double size?',
+			value: true,
+		}),
 		value: NumberSetting({
 			value: 100,
 			label: 'Value',
