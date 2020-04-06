e4cfc3656a4bfe49aeda61cedc3abc5d8569b19c
Jose Albizures <albizures3601@gmail.com>
2020-04-06 20:37:23 +0300

    docs(examples): improve state example

--- examples/src/sketches/state.ts
+++ examples/src/sketches/state.ts
@@ -1,30 +1,58 @@
 import { create, useContext, useMutableState, useState } from 'pieza';
 
+interface Circle {
+	x: number;
+	y: number;
+	radio: number;
+}
+
 interface State {
+	circles: Circle[];
 	x: number;
 	y: number;
 }
+
+const speed = 1;
 
 export default create<null, State>({
 	name: 'Using state',
 	autoClean: true,
 	setup() {
 		return {
+			circles: [],
 			x: 0,
 			y: 0,
 		};
 	},
+	update({ circles }) {
+		for (const circle of circles) {
+			circle.radio += speed;
+		}
+
+		return {
+			circles,
+		};
+	},
 	draw() {
 		const context = useContext();
-		const { x, y } = useState<State>();
-		context.circle(x, y, 40);
+		const { x, y, circles } = useState<State>();
+
+		for (const circle of circles) {
+			context.circle(circle.x, circle.y, circle.radio);
+		}
 	},
 	mouseMoved() {
 		const context = useContext();
-		const [, setState] = useMutableState();
-		setState({
+		const [state, setState] = useMutableState<State>();
+		const circle = {
+			radio: 10,
 			x: context.mouseX,
 			y: context.mouseY,
+		};
+
+		setState({
+			...state,
+			circles: state.circles.concat(circle),
 		});
 	},
 });
